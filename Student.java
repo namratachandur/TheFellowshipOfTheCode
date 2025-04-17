38127bad709f9c3238ddef863c9d3b5bd68d4b26
Namrata Chandur <chandur.namratanc@gmail.com>
2025-04-17 19:35:04 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
-public class Student extends Person 
+public class Student extends Person // This is a subclass of Person
 {   
-    private String school;
+    private String school; // Data field used in the Student class
 
     public Student(String name, int age, String school)
     {
@@ -16,11 +16,13 @@ public class Student extends Person
     @Override
     public void introduce()
     {
+        // This method overrides the introduce method in the Person class
         System.out.println("\nHello! My name is " + getName() + ", I am " + getAge() + " years old and I study at " + school + ".");
     }
 
     public static void main(String[] args) 
     {
+        // Tests instances of Student
         Student student = new Student("Frodo", 33, "The House of Lore");
         student.introduce();
     }
