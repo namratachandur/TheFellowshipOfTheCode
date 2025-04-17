4258dc973ff845faa455df084c18e03e4744326a
Namrata Chandur <chandur.namratanc@gmail.com>
2025-04-17 19:35:36 -0400

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,6 +1,6 @@
-public class Manager extends Employee 
+public class Manager extends Employee // This is a subclass of Employee (which is a subclass of Person)
 {
-    private int team_size;
+    private int team_size; // Data field used in the Manager class
 
     public Manager(String name, int age, double salary, int team_size) 
     {
@@ -16,12 +16,14 @@ public class Manager extends Employee
     @Override
     public void displaySalary()
     {
+        // This method overrides the displaySalary method in the Employee class
         System.out.println("I managing a team of " + team_size + " members.");
         System.out.println("I earn " + getSalary() + " silver coins per month.");
     }
 
     public static void main(String[] args) 
     {
+        // Tests instances of Manager
         Manager wizard = new Manager("Gandalf the Grey", 2019, 1000, 8);
         wizard.introduce();
         wizard.displaySalary();
