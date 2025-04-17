78b688102d3d6610250f2983a78e3df6cf88a1d8
Namrata Chandur <chandur.namratanc@gmail.com>
2025-04-17 19:35:20 -0400

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,6 +1,6 @@
-public class Employee extends Person
+public class Employee extends Person // This is a subclass of Person
 {
-    private double salary;
+    private double salary; // Data field used in the Employee class
 
     public Employee(String name, int age, double salary) 
     {
@@ -15,11 +15,13 @@ public class Employee extends Person
 
     public void displaySalary() 
     {
+        // This method displays the salary of the employee
         System.out.println("I earn " + salary + " silver coins per month.");
     }
 
     public static void main(String[] args)
     {
+        // Tests instances of Employee
         Employee archer = new Employee("Aragorn", 87, 150);
         archer.introduce();
         archer.displaySalary();
