e71b3e507399e2f5063ce8d261c301fea9a43254
Namrata Chandur <chandur.namratanc@gmail.com>
2025-04-17 19:25:38 -0400

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,36 @@
+public class Person
+{
+    private String name;
+    private int age;
+
+    public Person(String name, int age) 
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public int getAge() 
+    {
+        return age;
+    }
+
+    public void introduce()
+    {
+        System.out.println("\nHello! My name is " + name + " and I am " + age + " years old.");
+    }
+
+    public static void main(String[] args) 
+    {
+        Person elven_queen = new Person("Galadriel", 8000);
+        elven_queen.introduce();
+        Person hobbit = new Person("Bilbo", 111);
+        hobbit.introduce();
+        Person dwarf = new Person("Gimli", 140);
+        dwarf.introduce();
+    }
+}
